b74b03103878a06854507a419a9be8a4bdae8995
SO HUI <haejuuun925@gmail.com>
2023-11-20 19:03:19 +0900

    feat: Form 및 Input 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import SimpleInput from './components/SimpleInput';
 
 function App() {
   return (
     <div className="App">
-      
+      <SimpleInput />
     </div>
   );
 }
