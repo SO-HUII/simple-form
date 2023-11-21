b6aea881423c2e351be5c7c4d16aac37ccd9df89
SO HUI <haejuuun925@gmail.com>
2023-11-21 18:27:55 +0900

    feat: form 제출 처리 및 사용자 입력값 가져오기

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 const SimpleInput = (props) => {
+    const [enteredName, setEnteredName] = useState('');
+    const nameInputRef = useRef();
+
+    const nameInputChangeHandler = event => {
+        setEnteredName(event.target.value);
+    };
+
+    const formSubmissionHandler = event => {
+        event.preventDefault();
+
+        console.log(enteredName);
+        const enteredValue = nameInputRef.current.value;
+        console.log(enteredValue);
+
+        // nameInputRef.current.value = '';  // 권장되지 않음
+        setEnteredName('');
+    };
+
     return (
-        <form>
+        <form onSubmit={formSubmissionHandler}>
             <div className='form-control'>
                 <label htmlFor='name'>Your name</label>
-                <input type='text' id='name' />
+                <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} value={enteredName} />
             </div>
             <div className='form-actions'>
                 <button>Submit</button>
